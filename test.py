bd6c4e9770fdba524b64c6f051e884f5882203a1
jlchain <joseluis.munozchain@telefonica.com>
2025-11-26 09:01:37 +0100

    Prueba

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 from tests.api_tests import *
 from tests.feature_validation_tests import *
 # from tests.feature_validation_tests_somap import *
-
+# 
 
 if __name__ == '__main__':
     # run all imported test cases
